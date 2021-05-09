ca0485caa604f5c2413f869ecd3b8edbebc2c25e
Eva Harris <mail@gastrodon.io>
2021-05-08 21:38:00 -0400

    Export server_configs Channels
    Exported with a _prefix to disambiguate with channels/Channels

--- src/client_data/mod.rs
+++ src/client_data/mod.rs
@@ -3,5 +3,6 @@ mod server_configs;
 mod verify;
 
 pub use channels::{Channels, ChannelsKey};
+pub use server_configs::Channels as _Channels;
 pub use server_configs::{ServerConfig, ServerConfigKey, ServerConfigTable};
 pub use verify::{Verify, VerifyKey};
