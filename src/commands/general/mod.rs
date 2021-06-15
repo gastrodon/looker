b2d97327d9a8e543b626c597917157cab9e748dd
Eva Harris <mail@gastrodon.io>
2021-06-14 21:39:41 -0400

    Create -allow/deny handler stubs
    Create stubs for allowing / denying a verification candidate from
    quarantine
    
    Before these stubs do anything, we need to make the existing verify code
    reachable. DRY!

--- src/commands/general/mod.rs
+++ src/commands/general/mod.rs
@@ -2,10 +2,12 @@ use serenity::framework::standard::macros::group;
 
 mod about;
 mod unverified;
+mod verify;
 
 use about::ABOUT_COMMAND;
 use unverified::UNVERIFIED_COMMAND;
+use verify::{ACCEPT_COMMAND, DENY_COMMAND};
 
 #[group]
-#[commands(about, unverified)]
+#[commands(about, unverified, accept, deny)]
 pub struct General;
---
+++ src/commands/general/verify.rs
@@ -0,0 +1,47 @@
+use crate::{
+    client_data::{ServerConfig, ServerConfigKey},
+    config_for, maybe,
+};
+use serenity::{
+    framework::standard::{macros::command, Args, CommandResult},
+    model::{channel::Message, id::ChannelId},
+    prelude::Context,
+    Result,
+};
+
+async fn gate(ctx: &Context, message: &Message) -> Result<bool> {
+    let channel_id = message.channel_id;
+    let config = match channel_id.to_channel(&ctx.http).await?.guild() {
+        Some(guild) => config_for!(guild.id, ctx.data.read().await),
+        None => return Ok(false),
+    };
+
+    Ok(config.channels.quarantine.contains(&channel_id))
+}
+
+async fn cleanup(ctx: &Context, channel_id: ChannelId) -> Result<()> {
+    match channel_id.delete(&ctx.http).await {
+        Ok(_) => Ok(()),
+        Err(why) => Err(why),
+    }
+}
+
+#[command]
+pub async fn accept(ctx: &Context, message: &Message, _: Args) -> CommandResult {
+    if !maybe!(gate(ctx, message).await, Result) {
+        return Ok(());
+    }
+
+    maybe!(cleanup(ctx, message.channel_id).await, Result);
+    Ok(())
+}
+
+#[command]
+pub async fn deny(ctx: &Context, message: &Message, _: Args) -> CommandResult {
+    if !maybe!(gate(ctx, message).await, Result) {
+        return Ok(());
+    }
+
+    maybe!(cleanup(ctx, message.channel_id).await, Result);
+    Ok(())
+}
